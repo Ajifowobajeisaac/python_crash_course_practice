cbbda72715bcfa2c2226b29bba591974328fa0f6
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-07-01 04:41:59 +0100

    Added multiples validator using input()

---
+++ python_crash_course/input_and_while_loops/multiple.py
@@ -0,0 +1,13 @@
+# Exercise on inputs
+
+# This program takes an input then verifies whether it's a multiple of 8.
+
+prompt = "Type a number:"
+
+number = input(prompt)
+number = int(number)
+
+if number % 8 == 0:
+    print("Bingo! This number is a multiple of eight")
+else:
+    print("Oh, not what we're looking for")
