e21df787a71838b4ea8b2c02b6582e8d1397ddd3
Isaac Ajifowobaje <77357512+Ajifowobajeisaac@users.noreply.github.com>
2023-07-02 23:57:13 +0100

    Update multiple.py
    Added logic for 'try again' and 'quit'

--- python_crash_course/input_and_while_loops/multiple.py
+++ python_crash_course/input_and_while_loops/multiple.py
@@ -10,4 +10,7 @@ number = int(number)
 if number % 8 == 0:
     print("Bingo! This number is a multiple of eight")
 else:
-    print("Oh, not what we're looking for")
+    print("Oh, not what we're looking for. Try again or quit with 'q'")
+    number = input(prompt)
+    if number == 'q':
+        print("Thank you for playing")
