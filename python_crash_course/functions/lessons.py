67fde8920240588169d39ce14412e9e45c20f10b
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-07-04 01:32:35 +0100

    Added folder for functions

---
+++ python_crash_course/functions/lessons.py
@@ -0,0 +1,13 @@
+# Demonstrates the use of functions
+
+
+def lessons():
+    """Displays some programming tip I've learnt"""
+    print("""\n1. When nameing a list, enusure it has a plural name so the lits
+        items can be singular\n2. The variable names should be thoughtful to ensure 
+          readability of code\n 3. It's better to for the code to be be easy to
+          read than easy to write (if it's a choice between the two)""")
+    
+
+
+lessons()
