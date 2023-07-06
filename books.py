f51663129d947b1f6853a36790773acfa2ef34de
Isaac Ajifowobaje <77357512+Ajifowobajeisaac@users.noreply.github.com>
2023-07-07 00:01:19 +0100

    Added book title function

---
+++ books.py
@@ -0,0 +1,15 @@
+# Function that prints a book title
+# Parameter - book title
+
+
+
+def book(title):
+    """Takes a book title and prints a statement"""
+    print(f"\nMy favorite book is {title}\n")
+
+
+prompt = "\nWhat is the name of your favorited book?\n"
+
+title_jake = input(prompt)
+
+book(title_jake)
