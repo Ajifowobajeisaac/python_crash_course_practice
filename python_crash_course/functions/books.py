e194bd40a9dd2b658ad5af2f5f069500aef6b710
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-07-09 18:23:08 +0100

    Added comments and positional arguments

--- python_crash_course/functions/books.py
+++ python_crash_course/functions/books.py
@@ -1,8 +1,13 @@
-# Function that prints a book title
-# Parameter - book title
+# A showcase of Functions and inputs (Paramenters and arguments)
+# Parameters - book title, gente
+
+# This function only defines one paramenter. so it takes only one argument
+def book_title(title):
+    """Takes a book title and prints a statement"""
+    print(f"\nMy favorite book is {title}")
 
 
-
+# This function takes two postional arguments
 def book(title, genre):
     """Takes a book title and prints a statement"""
     print(f"\nMy favorite book is {title}, it is a {genre} book\n")
@@ -15,6 +20,13 @@ title = input(prompt_title)
 prompt_genre = f'What genre is {title}?\n'
 genre = input(prompt_genre)
 
+
+# Two different ways to call this function. One takes user input while the other
+# has arguments fed directly into the function call 
 book(title, genre)
+book('Finley Road', 'Drama')
 
+# Argument Position matters with position arguments. If we switch up the positions
+# we would get wierd results
 
+book('Drama', 'Romeo and Juliet')
