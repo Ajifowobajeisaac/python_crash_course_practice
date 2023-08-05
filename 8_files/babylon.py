7b701530a0dd45daac3b444d24c70c8c0e2325a3
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-08-05 05:35:34 +0100

    Removed intermediary folder for simpler path usage

---
+++ 8_files/babylon.py
@@ -0,0 +1,11 @@
+'''A program to display how to manipulate files in python'''
+
+from pathlib import Path
+
+path = Path.cwd()
+# babylon = Path(f'{path}/8_files/the_richest_man_in_babylon.txt')
+# babylon_content = babylon.read_text()
+print(path)
+
+# print(babylon_content)
+
