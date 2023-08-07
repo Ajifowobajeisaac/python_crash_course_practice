fa4a28d9d5a71ddcf2a20e85ba3c58d2bf575e2a
Ajifowobajeisaac <ajifowobajeisaac@gmail.com>
2023-08-07 23:54:37 +0100

    Tested and improve file writing script

--- 8_files/babylon.py
+++ 8_files/babylon.py
@@ -3,9 +3,20 @@
 from pathlib import Path
 
 path = Path.cwd()
-# babylon = Path(f'{path}/8_files/the_richest_man_in_babylon.txt')
-# babylon_content = babylon.read_text()
-print(path)
+babylon = Path(f'{path}/8_files/the_richest_man_in_babylon.txt')
+babylon_update = Path(f'{path}/8_files/text_files/the_richest_man_in_babylon_upda'
+                      'ted.txt')
+babylon_content = babylon.read_text()
+babylon_content = (babylon_content.replace('[15]', ''))
+babylon_content = (babylon_content.replace('[2]', ''))
+babylon_content = (babylon_content.replace('[3]', ''))
 
 # print(babylon_content)
+babylon_tmp = ''
+for line in babylon_content.splitlines():
+    print(f'\n{line}\n')
+    babylon_tmp += '\n' + line + '\n'
 
+babylon_update.write_text(babylon_tmp)
+
+# print(len(babylon_content.splitlines()))
